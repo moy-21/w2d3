4bdec09980163791a1a07a3f8aeb13aed91e51cf
Moises Renteria <moy@Moisess-MacBook-Air.local>
2022-04-14 00:29:51 -0500

    first commit

---
+++ mod.py
@@ -0,0 +1,16 @@
+from math import pi
+
+def area_():
+    len_= int(input("Lenght: "))
+    width_=int(input("Width: "))
+    area = (len_ * width_)
+    return area
+
+def circum_eq():
+    radius_= input("What is the radius?: ")
+    rdi= float(radius_)
+    circum_ = (2.0 * pi * rdi)
+    return circum_
+
+
+
